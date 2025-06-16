4b459c20691fdf76ab1770f762207c3850b1a637
dhanuprys <dhanuprys@gmail.com>
2025-06-16 09:53:25 +0800

    fix: adjust gita sentence transformer

---
+++ app/infrastructure/searcher/gita_searcher.py
@@ -0,0 +1,46 @@
+import os
+import pickle
+from typing import List
+
+import faiss
+from sentence_transformers import SentenceTransformer
+
+from app.application.service.searcher import Searcher
+from app.domain.entity.gita_entity import GitaEntity
+
+
+class GitaSearcher(Searcher):
+    def __init__(self):
+        self.path_prefix = "data/model/"
+        self.model = SentenceTransformer("intfloat/multilingual-e5-base")
+        self.index = None
+        self.verse_meta: List[GitaEntity] = []
+
+    def builded(self):
+        return os.path.exists(self.path_prefix + "gita.index") and os.path.exists(
+            self.path_prefix + "gita_meta.pkl"
+        )
+
+    def build_index(self, gita: List[GitaEntity]):
+        print(gita)
+        texts = [f"passage: {v.vt_content}" for v in gita]
+        embeddings = self.model.encode(texts, convert_to_numpy=True)
+
+        self.index = faiss.IndexFlatL2(embeddings.shape[1])
+        self.index.add(embeddings)
+        self.verse_meta = gita
+
+        faiss.write_index(self.index, self.path_prefix + "gita.index")
+        with open(self.path_prefix + "gita_meta.pkl", "wb") as f:
+            pickle.dump(gita, f)
+
+    def load_index(self):
+        self.index = faiss.read_index(self.path_prefix + "gita.index")
+        with open(self.path_prefix + "gita_meta.pkl", "rb") as f:
+            self.verse_meta = pickle.load(f)
+
+    def search(self, query: str, top_k=3) -> List[GitaEntity]:
+        query = f"query: {query}"
+        q_emb = self.model.encode([query])
+        D, I = self.index.search(q_emb, top_k)
+        return [self.verse_meta[i] for i in I[0]]
